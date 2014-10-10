8418575786963b66066ebfa7fa4fa0ba006c4d84
Stephen Blott <stephen@liftoff.io>
2014-10-10 15:44:38 +0100

    add zsh highlighting.

--- clones/zsh-syntax-highlighting.zsh
+++ clones/zsh-syntax-highlighting.zsh
@@ -1,3 +1,4 @@
 #!/usr/bin/env zsh
 
-install -v -m 0444 zsh-syntax-highlighting.zsh ~/.zsh-syntax-highlighting.zsh
+# install -v -m 0444 zsh-syntax-highlighting.zsh ~/.zsh-syntax-highlighting.zsh
+true
